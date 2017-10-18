947e20307acbbaf17820bbbd4ff9f681e8f0bc04
Luciano Andrade <andrade.luciano@gmail.com>
2017-10-17 22:11:49 -0300

    added FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,8 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
 	    new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+
+	    new FOS\RestBundle\FOSRestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
